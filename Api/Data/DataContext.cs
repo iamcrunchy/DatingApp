f502169038b4f019184184cb893325885a673e68
Crunchy <quinn.jp@gmail.com>
2022-01-18 11:49:09 -0500

    Initial commit

---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
---
+++ Api/Data/Migrations/20220118163648_ChangedTableNameToUsers.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Data.Migrations
+{
+    public partial class ChangedTableNameToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppUsers",
+                table: "AppUsers");
+
+            migrationBuilder.RenameTable(
+                name: "AppUsers",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "AppUsers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppUsers",
+                table: "AppUsers",
+                column: "Id");
+        }
+    }
+}
